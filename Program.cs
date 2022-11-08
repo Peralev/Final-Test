9e768edf8379ec7142b286c266c8ee5c39794e3a
Elau <lol1.com@mail.ru>
2022-11-08 19:39:52 +0300

    add check

--- Program.cs
+++ Program.cs
@@ -10,3 +10,24 @@
 
 string[] massInput ={"Hello", "2", "world", ":-)"};
 string[] massResult= {};
+
+PrintArray(massInput);
+
+for (int i = 0; i < massInput.Length; i++)
+{
+    if(massInput[i].Length <= 3)
+    {
+        string [] massNewResult = new string[massResult.Length + 1];    
+        
+        for (int j = 0; j < massResult.Length; j++)
+        {
+            massNewResult[j] = massResult[j];        
+        }
+
+        massNewResult[massNewResult.Length - 1] = massInput[i];        
+
+        massResult = massNewResult;         
+    }
+}
+
+PrintArray(massResult);
