81b0173338ee82bf4cb7def61cf52846332b143a
Elau <lol1.com@mail.ru>
2022-11-08 19:38:06 +0300

    add print methode

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿string[] massInput ={"Hello", "2", "world", ":-)"};
+﻿void PrintArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+
+string[] massInput ={"Hello", "2", "world", ":-)"};
 string[] massResult= {};
